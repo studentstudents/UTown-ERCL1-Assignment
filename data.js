20d3eab9209ad01d89ae96ba9a5656a8669a95ec
jy <105411412+studentstudents@users.noreply.github.com>
2022-05-12 20:10:42 +0800

    UTown-ERCL1-Assignment
    Assignment

---
+++ data.js
@@ -0,0 +1,170 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-erc_level-1_starbucks",
+      "name": "ERC_Level 1_Starbucks",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.0998560461196476,
+        "pitch": -0.18852575220214618,
+        "fov": 1.3181347489192914
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5333534534906477,
+          "pitch": -0.16192919678073103,
+          "rotation": 5.497787143782138,
+          "target": "1-erc_level-1_nearescalator_photo_1512"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.8146711261439634,
+          "pitch": 0.05075832958285886,
+          "title": "Vending Machines",
+          "text": "Snacks and Drinks available"
+        },
+        {
+          "yaw": 0.3829396749524996,
+          "pitch": -0.17649823387990082,
+          "title": "Open study area",
+          "text": "Available for students 24/7"
+        }
+      ]
+    },
+    {
+      "id": "1-erc_level-1_nearescalator_photo_1512",
+      "name": "ERC_Level 1_NearEscalator_PHOTO_1512",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.3475366146321086,
+        "pitch": -0.0677236306748874,
+        "fov": 1.3181347489192914
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0054588176862467,
+          "pitch": -0.03359511178846475,
+          "rotation": 10.210176124166829,
+          "target": "0-erc_level-1_starbucks"
+        },
+        {
+          "yaw": -1.9275592510159818,
+          "pitch": 0.015611528616638282,
+          "rotation": 4.71238898038469,
+          "target": "2-erc_level-1_tembusu-tree"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.03277754180964,
+          "pitch": 0.017685507386930865,
+          "title": "Delivery area",
+          "text": "Pick up and drop off point"
+        },
+        {
+          "yaw": -2.666335408500549,
+          "pitch": -0.06168900512020414,
+          "title": "Tembusu Tree",
+          "text": "Within ERC"
+        }
+      ]
+    },
+    {
+      "id": "2-erc_level-1_tembusu-tree",
+      "name": "ERC_Level 1_Tembusu Tree",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -3.0164472089104244,
+        "pitch": -0.08668148312351853,
+        "fov": 1.3181347489192914
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.396104833613487,
+          "pitch": 0.13604832235393616,
+          "rotation": 5.497787143782138,
+          "target": "1-erc_level-1_nearescalator_photo_1512"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.85447597911754,
+          "pitch": 0.003140893719663751,
+          "title": "Tembusu Tree",
+          "text": "Within ERC"
+        },
+        {
+          "yaw": -1.3829267681913802,
+          "pitch": 0.29292749589249567,
+          "title": "Open study area",
+          "text": "With chairs and table"
+        }
+      ]
+    }
+  ],
+  "name": "UTown - ERC L1 - Assignment",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
